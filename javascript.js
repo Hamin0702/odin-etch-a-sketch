ebc80b09d079202025a7649fb9333056cc5a6e1f
Hamin Han <hamin0166@gmail.com>
2026-09-22 23:34:41 -0400

    Add a button for user input to regenerate the squares

--- javascript.js
+++ javascript.js
@@ -5,19 +5,36 @@ function randomColor() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-let gridSize = 16;
+function createGrid(gridSize) {
+    // Clear existing grid
+    main.innerHTML = "";
 
-for (let i = 0; i < gridSize; i++) {
-    const rowGroup = document.createElement("div");
-    rowGroup.classList.add("row");
+    for (let i = 0; i < gridSize; i++) {
+        const rowGroup = document.createElement("div");
+        rowGroup.classList.add("row");
 
-    for (let j = 0; j < gridSize; j++) {
-        const div = document.createElement("div");
-        div.classList.add("cell");
-        rowGroup.appendChild(div);
+        for (let j = 0; j < gridSize; j++) {
+            const div = document.createElement("div");
+            div.classList.add("cell");
+            rowGroup.appendChild(div);
+        }
+
+        main.appendChild(rowGroup);
     }
+}
 
-    main.appendChild(rowGroup);
+
+function promptUser() {
+    while (true) {
+        const userInput = prompt("Number of squares per side for the new grid");
+        const num = Number(userInput);
+
+        if (userInput.trim() !== "" && !isNaN(num) && num <= 100) {
+            return num;
+        }
+
+        alert("Please enter a valid number under 100.");
+    }
 }
 
 main.addEventListener("mouseover", (e) => {
@@ -27,3 +44,10 @@ main.addEventListener("mouseover", (e) => {
         e.target.style.opacity = cellOpacity - 0.1;
     }
 });
+
+const gridBtn = document.querySelector("button");
+gridBtn.addEventListener("click", () => {
+   createGrid(promptUser());
+});
+
+createGrid(16);
